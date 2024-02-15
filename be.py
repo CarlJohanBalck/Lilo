2b8b0c31e8a69a0f97029014d9b81a71eef08655
Carl Johan Balck <carljohanbalck@gmail.com>
2024-02-15 15:43:09 +0100

    no dependencies of sense hat for be

--- be.py
+++ be.py
@@ -2,11 +2,12 @@ import flask_cors
 from flask import Flask, request
 import configparser
 from flask_cors import CORS
-from lilo import LiloFick
 
 from datetime import datetime, date, timedelta
 import json
+import subprocess
 
+import calendar
 
 from config import (
     IP_ADDRESS,   
@@ -48,11 +49,23 @@ def GetStatus():
     
 @app.route('/LiloFick', methods=['POST'])
 def LiloGot():
-     LiloFick()
+     LiloFickBe()
      response = ["200"]
      return json.dumps(response)
 
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0", port=PORT_BE, use_reloader=True)
+	
+def LiloFickBe():
+	now = datetime.now() # current date and time	
+	date_time = now.strftime("%d/%m/%Y")	
+	config = configparser.ConfigParser()
+	my_date = date.today()
+	weekday = calendar.day_name[my_date.weekday()]
+	config.read('lastDate.ini')
+	config['DEFAULT']['last_date'] = weekday + " " + date_time
+	with open('lastDate.ini', 'w') as configfile:
+		config.write(configfile)
+	subprocess.call(['sh', '/home/pi/code/scripts/push_lilo.sh'])
 	
